ba77facc8377d3832f9119b552521ff31d7197ef
Nicco Ryan <niccoryan0@gmail.com>
2020-07-24 10:48:11 -0700

    Stashing

---
+++ Challenges/CSharp/FIFOAnimalShelter/FIFOAnimalShelter/Classes/Shelter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+using System.Xml.XPath;
+using StacksAndQueues;
+
+namespace FIFOAnimalShelter.Classes
+{
+    public class Shelter : Queue<Animal>
+    {
+        public Node<Animal> Front { get; set; }
+        public Node<Animal> Rear { get; set; }
+
+        public Shelter()
+        {
+            Rear = Front;
+        }
+        /// <summary> 
+        /// Add a new animal to the shelter
+        /// </summary>
+        /// <param name="pet">Pet to be added</param>
+        public void Enqueue(Animal pet)
+        {
+            Node<Animal> animal = new Node<Animal>(pet);
+
+            if (Front == null)
+            {
+                Front = animal;
+                Rear = animal;
+            }
+            else
+            {
+                Rear.Next = animal;
+                Rear = animal;
+            }
+        }
+
+        /// <summary>
+        /// Removes an animal of specified type from the shelter.
+        /// </summary>
+        /// <param name="type">Type of pet desired</param>
+        /// <returns>First pet in Shelter of specified type.</returns>
+        public Animal Dequeue(string type)
+        {
+            Node<Animal> temp = Front;
+            do
+            {
+                if (temp.Value.Species == type)
+                {
+                    Animal result = temp.Value;
+                    temp = temp.Next;
+                    return result;
+                }
+                else
+                {
+                    Temp.Dequeue();
+                }
+            } while (Front != temp);
+            return TheShelter.Dequeue();
+        }
+
+    }
+}
---
+++ Data-Structures/StacksAndQueues/StacksAndQueues/Queue.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues
+{
+    public class Queue<T>
+    {
+        public Node<T> Front { get; set; }
+        public Node<T> Rear { get; set; }
+
+        public Queue()
+        {
+            Rear = Front;
+        }
+
+        /// <summary>
+        /// Add a node to the end of the queue.
+        /// </summary>
+        /// <param name="val">Value to be placed in node</param>
+        public void Enqueue(T val)
+        {
+            Node<T> node = new Node<T>(val);
+
+            if (Front == null)
+            {
+                Front = node;
+                Rear = node;
+            }
+            else
+            {
+                Rear.Next = node;
+                Rear = node;
+            }
+        }
+
+        /// <summary>
+        /// Remove front node from queue
+        /// </summary>
+        /// <returns>Value of removed node</returns>
+        public T Dequeue()
+        {
+            if (IsEmpty())
+            {
+                throw new Exception("Empty queue");
+            }
+            T result = Front.Value;
+            Front = Front.Next;
+            return result;
+        }
+
+        /// <summary>
+        /// Return top value of a queue or throw exception if empty
+        /// </summary>
+        /// <returns>Value of top node in queue</returns>
+        public T Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new Exception("Empty queue");
+            }
+            return Front.Value;
+        }
+
+        /// <summary>
+        /// Check if a queue is empty
+        /// </summary>
+        /// <returns>Boolean for if queue is empty</returns>
+        public bool IsEmpty()
+        {
+            bool result = Front == null ? true : false;
+            return result;
+        }
+    }
+}
