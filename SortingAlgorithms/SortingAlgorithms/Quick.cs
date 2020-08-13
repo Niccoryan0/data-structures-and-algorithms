a18f2ca366428de5270c7108d5f7dddd49d5936d
Nicco Ryan <niccoryan0@gmail.com>
2020-08-12 20:27:20 -0700

    Wrote summary comments and tests

--- SortingAlgorithms/SortingAlgorithms/Quick.cs
+++ SortingAlgorithms/SortingAlgorithms/Quick.cs
@@ -7,11 +7,23 @@ namespace SortingAlgorithms
 {
     public class Quick
     {
-        public static int[] Sort(int[] arr)
+        /// <summary>
+        /// Overall handler, calls the initial Sort with inital left and right values
+        /// </summary>
+        /// <param name="arr">Array to be sorted</param>
+        /// <returns>Sorted array</returns>
+        public static int[] QuickSort(int[] arr)
         {
             return Sort(arr, 0, arr.Length - 1);
         }
 
+        /// <summary>
+        /// Handles the partitioning to find the pivot point and sorting based off the pivot
+        /// </summary>
+        /// <param name="arr">Array being sorted</param>
+        /// <param name="left">Left point of sort</param>
+        /// <param name="right">Right point of sort</param>
+        /// <returns>Sorted array</returns>
         public static int[] Sort(int[] arr, int left, int right)
         {
             if (left < right)
@@ -23,6 +35,13 @@ namespace SortingAlgorithms
             return arr;
         }
 
+        /// <summary>
+        /// Handles the partitioning, calls Swap for the numbers if they are found to be unsorted and returns the current position in the sort
+        /// </summary>
+        /// <param name="arr">Array being sorted</param>
+        /// <param name="left">Left point of sort</param>
+        /// <param name="right">Right point of sort</param>
+        /// <returns>Pivot position of array</returns>
         private static int Partition(int[] arr, int left, int right)
         {
             int pivot = arr[right];
@@ -41,6 +60,12 @@ namespace SortingAlgorithms
             return low + 1;
         }
 
+        /// <summary>
+        /// Swap individuals numbers within the array
+        /// </summary>
+        /// <param name="arr">Array to be sorted</param>
+        /// <param name="i">Right index to swap</param>
+        /// <param name="low">Left index to swap</param>
         private static void Swap(int[] arr, int i, int low)
         {
             int temp = arr[i];
