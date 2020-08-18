7939be4398a746c2fd660f4abbb30f6a105e8354
Nicco Ryan <niccoryan0@gmail.com>
2020-08-17 17:12:29 -0700

    Wrote tests and initial RepeatedWord method

---
+++ Challenges/CSharp/RepeatedWord/RepeatedWord/RepeatedWord.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace RepeatedWord
+{
+    public class RepeatedWord
+    {
+        public static string FindRepeat(string str)
+        {
+            List<string> tracker = new List<string>();
+            var removeCommas = Regex.Replace(str, ",", string.Empty);
+            foreach (string word in removeCommas.Split(' '))
+            {
+                if (tracker.Contains(word.ToLower()))
+                {
+                    return word;
+                }
+                tracker.Add(word.ToLower());
+            }
+            return "";
+        }
+
+
+    }
+}
