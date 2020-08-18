8bfe96c8177317da23beea40f652f6f23af14ebb
Nicco Ryan <niccoryan0@gmail.com>
2020-08-17 23:00:43 -0700

    Repeated words completted

---
+++ Challenges/CSharp/RepeatedWord/README.md
@@ -0,0 +1,44 @@
+# Repeated Words
+*Author: Nicco Ryan*
+
+---
+
+### Problem Domain
+Given any string, create methods to find the first repeated word, as well as to count all the words and order them by their rate of occurence.
+
+---
+
+### Inputs and Expected Outputs
+
+| Input | Expected Output |
+| :----------- | :----------- |
+| "It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York..." | "summer" |
+| "Once upon a time, there was a brave princess who..." | "a" |
+| "This sentence must contain no repeating words within it." | "" |
+
+
+---
+
+### Big O
+
+
+| Method | Time | Space |
+| :----------- | :----------- | :----------- |
+| FindRepeat | O(n^2) | O(n) |
+| CountAndOrderWords | O(n^2) | O(n) |
+
+
+
+---
+
+
+### Whiteboard Visual
+![Repeated Words Whiteboard](../../../assets/RepeatedWordsWB.png)
+
+
+---
+
+### Change Log
+1.0: *Problem completed* - 8/17/2020
+
+---
--- Challenges/CSharp/RepeatedWord/RepeatedWord/RepeatedWord.cs
+++ Challenges/CSharp/RepeatedWord/RepeatedWord/RepeatedWord.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
+using HashTable;
 
 namespace RepeatedWord
 {
@@ -9,9 +11,10 @@ namespace RepeatedWord
         public static string FindRepeat(string str)
         {
             List<string> tracker = new List<string>();
-            var removeCommas = Regex.Replace(str, ",", string.Empty);
+            var removeCommas = Regex.Replace(str, @"[^\w\s]", "");
             foreach (string word in removeCommas.Split(' '))
             {
+                // Can be done with a for loop checking each item if tracker if it equals the word, contains does the same under the hood
                 if (tracker.Contains(word.ToLower()))
                 {
                     return word;
@@ -21,6 +24,26 @@ namespace RepeatedWord
             return "";
         }
 
+        public static List<string> CountAndOrderWords(string str)
+        {
+            var removeCommas = Regex.Replace(str, ",", string.Empty);
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+
+            foreach (string word in removeCommas.Split(' '))
+            {
+                if (counts.TryGetValue(word, out int count))
+                {
+                    counts[word] = count++;
+                }
+                else
+                {
+                    counts.Add(word, 0);
+                }
+            }
+            var result = counts.OrderBy(x => x.Value).ToDictionary(key => key.Key, value => value.Value);
+            return result.Keys.ToList();
+
+        }
 
     }
 }
